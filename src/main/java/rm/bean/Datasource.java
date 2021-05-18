604f4d559cf1acae1c0fedbfd294574a01f5478d
Роман Квітницький <Kvitnytskyi.R@ms.sumdu.edu.ua>
2021-05-18 16:22:57 +0300

    Uses + Datasource

--- src/main/java/rm/bean/Datasource.java
+++ src/main/java/rm/bean/Datasource.java
@@ -2,6 +2,8 @@ package main.java.rm.bean;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import main.java.rm.service.Assertions;
+import main.java.rm.service.StringLogic;
 import org.apache.log4j.Logger;
 
 public class Datasource {
@@ -20,38 +22,50 @@ public class Datasource {
     }
 
     public void setSource(String source) {
+        Assertions.isNotNull(source, "Data source", logger);
+        StringLogic.isVisible(source, "Source", logger);
+        StringLogic.isWholeWord(source, "Source", logger);
 
+        this.source = source;
     }
 
     public void setUrl(String url) {
+        Assertions.isNotNull(url, "Data url", logger);
+        StringLogic.isVisible(url, "Data url", logger);
+        StringLogic.isWholeWord(url, "Data url", logger);
 
+        this.url.set(url);
     }
 
     public void setPort(String port) {
+        Assertions.isNotNull(port, "Data port", logger);
+        StringLogic.isVisible(port, "Data port", logger);
+        StringLogic.isWholeWord(port, "Data port", logger);
 
+        this.port.set(port);
     }
 
     public String getUrl() {
-        return null;
+        return url.get();
     }
 
     public String getProtocol() {
-        return null;
+        return protocol;
     }
 
     public String getSource() {
-        return null;
-    }
-
-    public StringProperty urlProperty() {
-        return null;
-    }
-
-    public StringProperty portProperty() {
-        return null;
+        return source;
     }
 
     public String getPort() {
-        return null;
+        return port.get();
+    }
+
+    public StringProperty urlProperty() {
+        return url;
+    }
+
+    public StringProperty portProperty() {
+        return port;
     }
 }
