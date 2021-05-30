6acd42c2cc797acf22ff778ee7f98a1b160ee160
Роман Квітницький <Kvitnytskyi.R@ms.sumdu.edu.ua>
2021-05-30 20:57:09 +0300

    table Teachers and the ability to change table data

--- src/main/java/rm/controller/AdminController.java
+++ src/main/java/rm/controller/AdminController.java
@@ -8,12 +8,16 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.ResourceBundle;
+import java.util.logging.Filter;
+import java.util.logging.LogRecord;
 
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -28,6 +32,7 @@ import javafx.util.Callback;
 import rm.Main;
 import rm.bean.Datasource;
 import rm.bean.RoomInfo;
+import rm.bean.TeacherInfo;
 import rm.bean.User;
 
 import javafx.stage.Stage;
@@ -47,19 +52,37 @@ public class AdminController implements Initializable {
     @FXML
     private TableView<RoomInfo> namesTable;
     @FXML
+    private TableView<TeacherInfo> tableViewNames;
+    @FXML
     private TableColumn<RoomInfo, String> idCol;
     @FXML
     private TableColumn<RoomInfo, String> nameCol;
     @FXML
     private TableColumn<RoomInfo, CheckBox> editCol;
+    @FXML
+    private TableColumn<TeacherInfo, String> namesCol;
+
+    @FXML
+    private TextField nameTextField;
+
+    @FXML
+    private TextField surnameTextField;
+
+    @FXML
+    private TextField patronymicTextField;
 
     private RTModifySQL getSql;
+
     private HashMap<Integer, RoomInfo> rooms;
+    private HashMap<Integer, TeacherInfo> teachers;
+
+    TeacherInfo temp;
 
     private double xOffSet = 0;
     private double yOffSet = 0;
 
     ObservableList<RoomInfo> NamesList = FXCollections.observableArrayList();
+    ObservableList<TeacherInfo> TeachersList = FXCollections.observableArrayList();
 
     @Override
     public void initialize (URL url, ResourceBundle rb) {
@@ -73,7 +96,9 @@ public class AdminController implements Initializable {
             //user.setPassword("number1298UA");
 
             rooms = new HashMap<>();
+            teachers = new HashMap<>();
         }
+        //text();
         loadDate();
         makeStageDragable();
     }
@@ -106,12 +131,18 @@ public class AdminController implements Initializable {
     private void refreshTable() {
         NamesList.clear();
         NamesList.addAll(rooms.values());
+
+        TeachersList.clear();
+        TeachersList.addAll(teachers.values());
     }
 
     private void loadDate() {
+
         try {
             getSql.getProvider().connect();
             getSql.getRooms(rooms, null);
+            getSql.getTeachers(teachers);
+
         } catch (SQLException e) {
             logger.warn("Rooms get error: " + e.getMessage());
         }
@@ -121,7 +152,23 @@ public class AdminController implements Initializable {
 
         namesTable.setItems(NamesList);
 
+        namesCol.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<TeacherInfo, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<TeacherInfo, String> p) {
+                SimpleStringProperty smp = new SimpleStringProperty(p.getValue().getSurname() + " " +
+                        p.getValue().getName() + " " + p.getValue().getPatronymic());
+                return smp;
+            }
+        });
+        tableViewNames.setItems(TeachersList);
+
+
     }
+
+
+
+
+
 
     private void makeStageDragable() {
         parent.setOnMousePressed((event) -> {
@@ -139,5 +186,35 @@ public class AdminController implements Initializable {
         parent.setOnMouseReleased((event) -> {
             Main.stage.setOpacity(1.0f);
         });
+    }
+
+    @FXML
+    private void addNewName(MouseEvent mouseEvent) {
+
+    }
+
+
+    public void selectContractContractTab(MouseEvent mouseEvent) {
+
+
+        temp = tableViewNames.getSelectionModel().getSelectedItem();
+        nameTextField.setText(temp.getName());
+        surnameTextField.setText(temp.getSurname());
+        patronymicTextField.setText(temp.getPatronymic());
+
+        renameName(null);
+    }
+
+    public void renameName(ActionEvent actionEvent) {
+        String name = nameTextField.getText();
+        String surname = surnameTextField.getText();
+        String patronymic = patronymicTextField.getText();
+        //System.out.println(temp.getName());
+        temp.setName(name);
+        temp.setSurname(surname);
+        temp.setPatronymic(patronymic);
+
+        refreshTable();
+        //System.out.println(temp.getName());
     }
 }
