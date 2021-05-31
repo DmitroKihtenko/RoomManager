bdbfa3b77b081be710e5b5049ead60f34baba91b
Роман Квітницький <Kvitnytskyi.R@ms.sumdu.edu.ua>
2021-05-31 23:35:15 +0300

    EditController selectedTeacher, selectedRoom, selectedHousing

--- src/main/java/rm/controller/EditController.java
+++ src/main/java/rm/controller/EditController.java
@@ -12,8 +12,10 @@ import java.util.logging.Filter;
 import java.util.logging.LogRecord;
 
 import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -30,16 +32,14 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.Callback;
 import rm.Main;
-import rm.bean.Datasource;
-import rm.bean.RoomInfo;
-import rm.bean.TeacherInfo;
-import rm.bean.User;
+import rm.bean.*;
 
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import rm.database.mySql.RTGetSQL;
 import org.apache.log4j.Logger;
 import rm.database.mySql.RTModifySQL;
+import rm.service.Assertions;
 import rm.service.Beans;
 
 public class EditController {
@@ -56,17 +56,69 @@ public class EditController {
     private TextField patronymicTextField;
 
     @FXML
+    private TextField numberTextField;
+
+    private ConnectionsList rtAccess;
+    ObjectProperty<TeacherInfo> selectedTeacher;
+    ObjectProperty<RoomInfo> selectedRoom;
+    ObjectProperty<HousingInfo> selectedHousing;
+
+    @FXML
     public void initialize() {
+        if (selectedTeacher == null) {
+            selectedTeacher = (ObjectProperty<TeacherInfo>) Beans.context().get("selectedTeacher");
+            selectedTeacher.addListener(new ChangeListener<TeacherInfo>() {
+                @Override
+                public void changed(ObservableValue<? extends TeacherInfo> observableValue,
+                                    TeacherInfo teacherInfo, TeacherInfo t1) {
+                    if (t1 != null && !t1.equals(teacherInfo)) {
+                        nameTextField.setText(t1.getName());
+                        surnameTextField.setText(t1.getSurname());
+                        patronymicTextField.setText(t1.getPatronymic());
+                    }
+                }
+            });
+            nameTextField.textProperty().addListener(new ChangeListener<String>() {
+                @Override
+                public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
+                    if (!t1.equals(s)) {
+                        selectedTeacher.get().setName(t1);
+                    }
+                }
+            });
+        }
 
+        if (selectedRoom == null) {
+            selectedRoom = (ObjectProperty<RoomInfo>) Beans.context().get("selectedRoom");
+            selectedRoom.addListener(new ChangeListener<RoomInfo>() {
+                @Override
+                public void changed(ObservableValue<? extends RoomInfo> observableValue,
+                                    RoomInfo roomInfo, RoomInfo t1) {
+                    if (t1 != null && !t1.equals(roomInfo)) {
+                        numberTextField.setText(t1.getNumber());
+                    }
+                }
+            });
+        }
+
+        /*
+        if (selectedHousing == null) {
+            selectedHousing = (ObjectProperty<HousingInfo>) Beans.context().get("selectedHousing");
+            selectedHousing.addListener(new ChangeListener<HousingInfo>() {
+                @Override
+                public void changed(ObservableValue<? extends HousingInfo> observableValue,
+                                    HousingInfo housingInfo, HousingInfo t1) {
+                    if (t1 != null && !t1.equals(housingInfo)) {
+                        numberTextField.setText(t1.getName());
+                    }
+                }
+            });
+        }
+         */
     }
 
-    @FXML
-    private void selectContractContractTab(MouseEvent mouseEvent) {
-        
-    }
-
-    @FXML
-    private void renameName(ActionEvent actionEvent) {
-
+    public void setEditTeachers(ConnectionsList rtAccess) {
+        Assertions.isNotNull(rtAccess, "Access connections", logger);
+        this.rtAccess = rtAccess;
     }
 }
