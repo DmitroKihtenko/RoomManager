217ee12e27b7971102bf39ef39cb48b1addcf47c
Kihtenko Dmitro <dima2015kistenko@gmail.com>
2021-06-04 21:07:10 +0300

    Changed main method and controllers logic

--- src/main/java/rm/controller/RoomsTableController.java
+++ src/main/java/rm/controller/RoomsTableController.java
@@ -2,17 +2,14 @@ package rm.controller;
 
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import org.apache.log4j.Logger;
-import rm.model.ConnectionsList;
 import rm.model.HousingInfo;
+import rm.model.Room;
 import rm.model.RoomInfo;
 import rm.service.Assertions;
 import rm.service.Beans;
@@ -24,122 +21,39 @@ import java.util.regex.Pattern;
 public class RoomsTableController {
     private static final Logger logger =
             Logger.getLogger(RoomsTableController.class);
-    private static final String DEF_ROOM_NAME = "0";
     private static final String SEPARATOR = "-";
     @FXML
-    private TableView<RoomInfo> roomsTable;
+    private TableView<Room> roomsTable;
     @FXML
-    private TableColumn<RoomInfo, String> roomNameCol;
+    private TableColumn<Room, String> roomNameCol;
     @FXML
     private TextField searchField;
-    private HashMap<Integer, RoomInfo> rooms;
+    private HashMap<Integer, Room> rooms;
     private HashMap<Integer, HousingInfo> housings;
-    private ConnectionsList rtAccess;
-    private final ObjectProperty<RoomInfo> selectedRoom;
-
-    private final ChangeListener<Object> refreshListener;
-    private final ChangeListener<Number> housingsId;
-    private final HashMap<Integer, Integer> roomsForHousings;
+    private final ObjectProperty<Room> selectedRoom;
 
     /**
      * Default constructor. Object initialization
      */
     public RoomsTableController() {
-        selectedRoom = (ObjectProperty<RoomInfo>)
+        selectedRoom = (ObjectProperty<Room>)
                 Beans.context().get("selectedRoom");
-        refreshListener = (observableValue, o, t1) ->
-                roomsTable.refresh();
-        housingsId = new ChangeListener<>() {
-            @Override
-            public void changed(ObservableValue<? extends Number>
-                                        observableValue,
-                                Number oldV,
-                                Number newV) {
-                Integer integer = oldV.intValue();
-                Integer t1 = newV.intValue();
-                if(integer == Integer.MIN_VALUE) {
-                    integer = null;
-                }
-                if(t1 == Integer.MIN_VALUE) {
-                    t1 = null;
-                }
-
-                HousingInfo housing;
-                if(integer == null && t1 != null) {
-                    housing = housings.get(t1);
-                    if(housing != null) {
-                        if(!roomsForHousings.containsKey(t1)) {
-                            roomsForHousings.put(t1, 1);
-                        } else {
-                            roomsForHousings.put(t1,
-                                    roomsForHousings.get(t1) + 1);
-                        }
-                        housing.nameProperty().
-                                removeListener(refreshListener);
-                        housing.nameProperty().
-                                addListener(refreshListener);
-                    }
-                } else if(integer != null && t1 == null) {
-                    housing = housings.get(integer);
-                    if(housing != null) {
-                        if(roomsForHousings.get(integer) <= 1) {
-                            roomsForHousings.remove(integer);
-                            housing.nameProperty().
-                                    removeListener(refreshListener);
-                        } else {
-                            roomsForHousings.put(integer,
-                                    roomsForHousings.get(integer) - 1);
-                        }
-                    }
-                } else if(t1 != null) {
-                    housing = housings.get(integer);
-                    if(housing != null) {
-                        if(roomsForHousings.get(integer) <= 1) {
-                            roomsForHousings.remove(integer);
-                            housing.nameProperty().
-                                    removeListener(refreshListener);
-                        } else {
-                            roomsForHousings.put(integer,
-                                    roomsForHousings.get(integer) - 1);
-                        }
-                    }
-                    housing = housings.get(t1);
-                    if(housing != null) {
-                        if(!roomsForHousings.containsKey(t1)) {
-                            roomsForHousings.put(t1, 1);
-                        } else {
-                            roomsForHousings.put(t1,
-                                    roomsForHousings.get(t1) + 1);
-                        }
-                        housing.nameProperty().
-                                removeListener(refreshListener);
-                        housing.nameProperty().
-                                addListener(refreshListener);
-                    }
-                }
-            }
-        };
-        roomsForHousings = new HashMap<>();
     }
 
     /**
      * Initialization room, housing, rtAccess, roomList
      * @param rooms cloned rooms
      * @param housings cloned housing
-     * @param rtAccess connection list
      */
-    public void setRooms(HashMap<Integer, RoomInfo> rooms,
-                         HashMap<Integer, HousingInfo> housings,
-                         ConnectionsList rtAccess) {
+    public void setRooms(HashMap<Integer, Room> rooms,
+                         HashMap<Integer, HousingInfo> housings) {
         Assertions.isNotNull(rooms, "Rooms map", logger);
         Assertions.isNotNull(housings, "Housings map", logger);
-        Assertions.isNotNull(rtAccess, "Access connections", logger);
 
         this.rooms = rooms;
         this.housings = housings;
-        this.rtAccess = rtAccess;
 
-        ObservableList<RoomInfo> roomsList = roomsTable.getItems();
+        ObservableList<Room> roomsList = roomsTable.getItems();
         roomsList.clear();
         roomsList.addAll(rooms.values());
     }
@@ -150,72 +64,6 @@ public class RoomsTableController {
     @FXML
     public void initialize() {
         if(selectedRoom != null) {
-            roomsTable.getItems().addListener((ListChangeListener
-                    <RoomInfo>) change -> {
-                Integer housingId;
-                HousingInfo housing;
-                while(change.next()) {
-                    if(change.wasAdded()) {
-                        for (RoomInfo room : change.
-                                getAddedSubList()) {
-                            housingId = room.getHousingId();
-                            room.numberProperty().
-                                    addListener(refreshListener);
-                            room.housingIdProperty().
-                                    addListener(refreshListener);
-                            room.housingIdProperty().
-                                    addListener(housingsId);
-                            housing = housings.get(housingId);
-                            if(housing != null) {
-                                housing.nameProperty().
-                                        removeListener(
-                                                refreshListener);
-                                housing.nameProperty().
-                                        addListener(
-                                                refreshListener);
-                                if(!roomsForHousings.containsKey(
-                                        housingId)) {
-                                    roomsForHousings.put(housingId,
-                                            1);
-                                } else {
-                                    roomsForHousings.put(housingId,
-                                            roomsForHousings.get(
-                                                    housingId) + 1);
-                                }
-                            }
-                        }
-                    } else if(change.wasRemoved()) {
-                        for (RoomInfo room : change.getRemoved()) {
-                            room.numberProperty().
-                                    removeListener(refreshListener);
-                            room.housingIdProperty().
-                                    removeListener(refreshListener);
-                            room.housingIdProperty().
-                                    removeListener(housingsId);
-                            housingId = room.getHousingId();
-                            housing = housings.get(housingId);
-                            if(housing != null) {
-                                housing.nameProperty().removeListener(
-                                        refreshListener);
-                                if(roomsForHousings.containsKey(
-                                        housingId)) {
-                                    roomsForHousings.put(housingId,
-                                            roomsForHousings.get(
-                                                    housingId) - 1);
-                                    if(roomsForHousings.get(housingId)
-                                            <= 0) {
-                                        room.housingIdProperty().
-                                                removeListener(
-                                                refreshListener);
-                                        roomsForHousings.remove(
-                                                housingId);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            });
             roomsTable.getSelectionModel().selectedItemProperty().
                     addListener((observableValue, roomInfo,
                                  t1) -> {
@@ -239,41 +87,13 @@ public class RoomsTableController {
     }
 
     /**
-     * Add new room in room list
-     */
-    @FXML
-    public void newRoom() {
-        ObservableList<RoomInfo> roomsList = roomsTable.getItems();
-        RoomInfo newRoom = new RoomInfo(DEF_ROOM_NAME);
-        newRoom.createUniqueId();
-        roomsList.add(newRoom);
-        rooms.put(newRoom.getId(), newRoom);
-        roomsTable.getSelectionModel().select(newRoom);
-    }
-
-    /**
-     * Delete selected room of room list
-     */
-    @FXML
-    public void deleteRoom() {
-        ObservableList<RoomInfo> roomsList = roomsTable.getItems();
-        RoomInfo roomToDelete = roomsTable.getSelectionModel().
-                getSelectedItem();
-        if(roomToDelete != null) {
-            rtAccess.removeSecondConnections(roomToDelete.getId());
-            roomsList.remove(roomToDelete);
-            rooms.remove(roomToDelete.getId());
-        }
-    }
-
-    /**
      * Search room in rooms list
      */
     public void searchRooms() {
         String text = searchField.getText().toLowerCase(Locale.ROOT);
-        ObservableList<RoomInfo> roomsList = roomsTable.getItems();
+        ObservableList<Room> roomsList = roomsTable.getItems();
         roomsList.clear();
-        for (RoomInfo room : rooms.values()) {
+        for (Room room : rooms.values()) {
             if(text.length() == 0) {
                 roomsList.add(room);
             } else {
